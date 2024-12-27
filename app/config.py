7250815c2d6c8e5ff98d29181afb92df7a0d329b
anh-khoa-nguyen <nak.khoanguyen@gmail.com>
2024-12-27 09:54:34 +0700

    commit20

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
-account_sid = 'ACfc1999b732d99aedbd784fac10a3ab25'
-auth_token = 'b15c2ce6f4f15bfa3d2ed46d6c5a8041'
-twilio_phone = '+12184894488'
-customer_no = '+84862465874'
+# account_sid = 'ACfc1999b732d99aedbd784fac10a3ab25'
+# auth_token = 'b15c2ce6f4f15bfa3d2ed46d6c5a8041'
+# twilio_phone = '+12184894488'
+# customer_no = '+84862465874'
